4128d44807f96702ceff0472b388785948512707
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-11-21 14:40:04 -0600

    update script -- file are bad

--- ABI_ana.sh
+++ ABI_ana.sh
@@ -1,13 +1,18 @@
 #!/bin/zsh
 
-minus_file="$1"
-plus_file="$2"
+
+
+grep "\-cyclus" ABI_full.txt > ABI_minus.txt
+grep "\+cyclus" ABI_full.txt > ABI_plus.txt
+
+minus_file="ABI_minus.txt"
+plus_file="ABI_plus.txt"
 
 while read -r line
 do
       
       #echo ${line:1} |cut -d'(' -f1 | sed -e 's/std::string/std::__cxx11::basic_string\<char, std::char_traits\<char\>, std::allocator\<char\> \>/g'
-      tofind=`echo ${line:1} |cut -d'(' -f1 | sed -e 's/std::string/std::__cxx11::basic_string\<char, std::char_traits\<char\>, std::allocator\<char\> \>/g' | sed -e 's/std::list/std::__cxx11::list/g' | sed -e 's/>>/> >/g'`
+      tofind=`echo ${line:1} |cut -d'(' -f1`
       #echo $tofind 
       echo $line
       grep $tofind $plus_file
